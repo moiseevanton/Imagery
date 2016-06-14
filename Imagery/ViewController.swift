67aabd30a5f97226ef958b82cdf3a38b44af1b99
moiseevanton <athidif94@gmail.com>
2016-06-14 10:07:25 -0400

    Initial commit

---
+++ Imagery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Imagery
+//
+//  Created by Anton Moiseev on 2016-06-14.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
