689922999f2d757b8fd4259c07e5efaf566f7756
jayeshcode <your@email.com>
2016-06-17 11:06:08 -0400

    custom camera

---
+++ Imagery/Album.swift
@@ -0,0 +1,13 @@
+//
+//  Album.swift
+//  Imagery
+//
+//  Created by Jayesh Wadhwani on 2016-06-16.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+import UIKit
+
+class Album: NSObject {
+
+}
---
+++ Imagery/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  Imagery
+//
+//  Created by Jayesh Wadhwani on 2016-06-14.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
