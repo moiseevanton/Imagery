18517f5e5b8990f32969ae6ae0d5b180b8e1d7a2
jayeshcode <your@email.com>
2016-06-22 10:42:11 -0400

    social adedd

---
+++ Imagery/ViewController+Social.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController+Social.swift
+//  Imagery
+//
+//  Created by Jayesh Wadhwani on 2016-06-20.
+//  Copyright © 2016 Anton Moiseev. All rights reserved.
+//
+
+import Foundation
